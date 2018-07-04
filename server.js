3d37f2150e0f0174ea55c77a1574e4317255cfc0
James Kropp <james-kropp@hotmail.com>
2018-07-03 21:04:35 -0700

    Adding heroku support

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 const request = require('request-promise');
 const _ = require('lodash');
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+require('dotenv').config();
 
 // Defined Routes
 
@@ -138,6 +140,15 @@ let league = {
     });
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
